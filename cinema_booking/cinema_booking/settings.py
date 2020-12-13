39b3f947cb376029b53b50a3e54e9ececf763333
Alexander Andreev <1996sasha2507@mail.ru>
2020-12-13 17:27:48 +0300

    Test Git Fork

--- cinema_booking/cinema_booking/settings.py
+++ cinema_booking/cinema_booking/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'cinema_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'booking_cinema_db',
+        'USER': 'custom_user',
+        'PASSWORD': 'w3qxst1ck',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
